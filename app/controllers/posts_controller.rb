21a84c104155777f6d5762b9218ab33acaecffec
ShangMing Cherng <night1300013@gmail.com>
2018-01-06 23:20:41 -0800

    assignment-17-rails-CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   def index
+    Post.all.each do |post|
+      if post.id % 5 == 1
+        post.title = "SPAM"
+        post.save
+      end
+    end
     @posts = Post.all
   end
 
