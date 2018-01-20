1c8af932f183be429cdd0687d9b713215af2b087
ShangMing Cherng <night1300013@gmail.com>
2018-01-19 23:52:46 -0800

    change to passing lambda in conditional option

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 },\
-   if: "password_digest.nil?"
+   if: ->{ password_digest.nil? }
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
             presence: true,
