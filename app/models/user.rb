c4cebfa3ede7d9835d8e2b025c6c5c81f2d95e2a
ShangMing Cherng <night1300013@gmail.com>
2018-01-20 19:01:21 -0800

    checkpoint-27-rails-authorization:completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
-  before_save {
-    if name.present?
-      name_array = []
-      name.split.each do |n|
-        name_array << n.capitalize
-      end
-      self.name = name_array.join(" ")
-    end
-  }
+  before_save { self.role ||= :member }
+  before_save :format_name
+
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 },\
@@ -21,5 +15,15 @@ class User < ApplicationRecord
             length: { minimum:3, maximum: 254 }
 
   has_secure_password
+  enum role: [:member, :admin]
 
+  def format_name
+    if name.present?
+      name_array = []
+      name.split.each do |n|
+        name_array << n.capitalize
+      end
+      self.name = name_array.join(" ")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118055357) do
+ActiveRecord::Schema.define(version: 20180121011947) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180118055357) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
