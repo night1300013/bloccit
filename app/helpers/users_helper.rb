271d5b5220ac96bcdb993ca5c1ed6e0c4b2c5739
ShangMing Cherng <night1300013@gmail.com>
2018-01-24 00:51:36 -0800

    Don't know how to TDD the show.html.erb that shows a list of favorited post

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def has_posts?(user)
+    user.posts.count == 0 && user.comments.count == 0
+  end
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :comment do
+    body RandomData.random_sentence
+    user
+  end
+end
