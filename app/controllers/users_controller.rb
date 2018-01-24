7e8a00a83b52127d6947d00235d03900aa5e7760
ShangMing Cherng <night1300013@gmail.com>
2018-01-23 23:05:14 -0800

    checkpoint-30-rails-public-profiles:completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.visible_to(current_user)
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,11 @@ class User < ApplicationRecord
     favorites.where(post_id: post.id).first
   end
 
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
   private
   def format_name
     if name.present?
