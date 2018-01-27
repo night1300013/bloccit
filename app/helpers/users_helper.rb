daa3a7179d0eb60a73b914be824ebf8a1d6d9cab
ShangMing Cherng <night1300013@gmail.com>
2018-01-27 12:43:36 -0800

    add user_helper collection doesn't work

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,13 @@
 module UsersHelper
   def has_posts?(user)
-    user.posts.count == 0 && user.comments.count == 0
+    user.posts.count != 0 || user.comments.count != 0
+  end
+
+  def favorite_user(favorite)
+    favorite.post.user
+  end
+
+  def favorite_post(favorite)
+    favorite.post
   end
 end
