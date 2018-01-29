33acab3e79ad03f0709c3c49ea2400148627f605
ShangMing Cherng <night1300013@gmail.com>
2018-01-28 21:50:05 -0800

    change to collection and delegate

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,11 +3,4 @@ module UsersHelper
     user.posts.count != 0 || user.comments.count != 0
   end
 
-  def favorite_user(favorite)
-    favorite.post.user
-  end
-
-  def favorite_post(favorite)
-    favorite.post
-  end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  delegate :topic, to: :post, prefix: true
+  delegate :user, to: :post, prefix: true
 end
